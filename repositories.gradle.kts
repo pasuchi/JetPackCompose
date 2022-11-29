9bcf18a4eab5c63ebe208a1932277fd4b3cfb1c6
Marco <marco@Marcos-MacBook-Pro.local>
2022-11-28 23:01:23 -0500

    feat: add version catalog with source control
    Upgrade version gradle to 7.5.1

---
+++ repositories.gradle.kts
@@ -0,0 +1,5 @@
+sourceControl {
+    gitRepository(uri("/Users/marco/Github/mmaquera/version-catalog")) {//uri("https://github.com/mmaquera/version-catalog.git")) {
+        producesModule("com.mmaquera:version-catalog-plugin")
+    }
+}
